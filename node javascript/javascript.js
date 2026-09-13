0d144fc4996ae1e4aa2a90d34170979ab3c5f07f
wieslawpaleta <tankluk@gmail.com>
2026-09-13 20:44:35 +0200

    Add files via upload

---
+++ node javascript/javascript.js
@@ -0,0 +1,28 @@
+// JavaScript, Where To, Introduction, Where To, Output, Syntax, Statements  (tu jestem teraz)
+
+
+let x = 5;
+let y = 6;
+let u = 10.50;
+
+let z = x + y;
+let w = x + y + u;
+
+const fname = "John";
+
+console.log(z + " " + "Coucou")
+console.log(w)
+
+let p;
+p = 6;
+
+console.log(fname + p)
+
+console.log((5 + 6) * 10)
+
+let l, k, j;
+l = 1;
+k = 2;
+j = l + k;
+console.log(j)
+
