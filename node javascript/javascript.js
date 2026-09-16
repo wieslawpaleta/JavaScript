3e5e79f1da957a15dac97c5c9417af30178a2633
wieslawpaleta <tankluk@gmail.com>
2026-09-16 20:49:18 +0200

    Add files via upload

--- node javascript/javascript.js
+++ node javascript/javascript.js
@@ -1,4 +1,4 @@
-// JavaScript, Where To, Introduction, Where To, Output, Syntax, Statements, Comments, Variables  (tu jestem teraz)
+// JavaScript, Where To, Introduction, Where To, Output, Syntax, Statements, Comments, Variables, Let, Const  (tu jestem teraz)
 /* */
 
 let x = 5;
@@ -26,9 +26,58 @@ k = 2;
 j = l + k;
 console.log(j)
 
+
+let _lastName = "Pierre";
+let _x = 2;
+let _100 = 5;
+let $ = "Bonsoir!"
+let voitureNombre;
+voitureNombre = "Peugeot";
+
+
+const prix1 = 5;
+const prix2 = 6;
+let total = prix1 + prix2;
+
+
+let personne = "Jean Pierre", nobreVoiture = "Citroen", prix3 = 300;
+
+
+let op = 2 + 3 + "5";
+
+console.log(op);
+
+var gr = 10;
+
+{
+var gr = 11;
+}
+
+console.log(gr);
+
+
+carName = "Volvo";
+var carName;
+
 //function myFunction() {
 //   document.getElementById("demo1").innerHTML = "Hello Dolly!";
 //   document.getElementById("demo2").innerHTML = "How are you?";
 // }
 
 // console.log(myFunction())
+
+
+const voitureConst = ["Peugeot", "Citroen", "Renault"];
+voitureConst[0] = "DS";
+voitureConst.push("Alpina");
+
+console.log(voitureConst);
+
+
+const voitureObject = {type:"Peugeot", model:"3008", couleur: "blanc"};
+
+voitureObject.couleur = "rouge";
+
+voitureObject.proprietaire = "Jacquouille la Fripouille";
+
+console.log(voitureObject);
