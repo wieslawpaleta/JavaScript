8e8b6d9f1e296d6ca5c6868bfd07f77f4d513971
wieslawpaleta <tankluk@gmail.com>
2026-09-19 20:54:52 +0200

    Add files via upload

--- node javascript/javascript.js
+++ node javascript/javascript.js
@@ -1,4 +1,4 @@
-// JavaScript, Where To, Introduction, Where To, Output, Syntax, Statements, Comments, Variables, Let, Const  (tu jestem teraz)
+// JavaScript, Where To, Introduction, Where To, Output, Syntax, Statements, Comments, Variables, Let, Const, Datatypes, Operators, Arithmetic  (tu jestem teraz)
 /* */
 
 let x = 5;
@@ -81,3 +81,136 @@ voitureObject.couleur = "rouge";
 voitureObject.proprietaire = "Jacquouille la Fripouille";
 
 console.log(voitureObject);
+
+
+const ao = 10;
+
+{
+const ao = 2;
+console.log(ao);
+}
+
+console.log(ao);
+
+var bo = 2;
+var bo = 3;
+bo = 4;
+
+console.log(bo);
+
+
+let co = "jaune";
+let eo = 16;
+let fo = 7.5;
+let go = 1234567890123456789012345n;
+let ho = BigInt(1234567890123456789012345);
+let io = true;
+let jo = false;
+const ko = {nom:"Obelix", nomDeFamille:"Asterix"};
+const lo = ["Peugeot", "Renault", "Citroen"];
+const mo = new Date("2022-03-25");
+let no;
+let oo;
+let po = null;
+let qo = null;
+const ro = Symbol();
+const so = Symbol();
+
+console.log(so);
+
+
+typeof ""
+typeof "Gabriel"
+typeof "Dubois  Dubois"
+typeof 0
+typeof 314
+typeof 3.14
+typeof (3)
+typeof (3 + 4)
+
+
+let to = 123e5;
+console.log(to);
+
+
+let uo = 5;
+
+(uo == 8);
+(uo != 8);
+(uo > 8);
+(uo < 8);
+
+
+let vo;
+let wo = "";
+
+
+let xo = 10;
+let yo = 2;
+let zo = xo + yo;
+
+console.log(zo);
+
+
+let ai = 5;
+let bi = 2;
+let ci = ai * bi;
+
+
+let di = 3;
+let ei = (100 * 50) * di;
+
+
+let fi = "Jean";
+let gi = "Rogue";
+let hi = fi + " " + gi;
+
+console.log(hi);
+
+
+let ii = "Quelle ";
+ii += "belle journée !";
+console.log(ii)
+
+
+let ji = 5 + 5;
+let ki = "5" + 5;
+let li = "Coucou" + 5;
+console.log(ji);
+console.log(ki);
+console.log(li);
+
+
+let mi = 10;
+mi += 5;
+console.log(mi);
+
+
+let ni = 5;
+let resultat = ni > 8;
+console.log(resultat);
+
+
+let oi = "A";
+let pi = "B";
+let resultat1 = oi === pi;
+console.log(resultat1);
+
+
+let qi = mi % ni ;
+console.log(qi);
+
+
+// let ri = 5;
+// ri++;
+// let si = ri;
+// console.log(si);
+
+// let ri = 5;
+// ri--;
+// let si = ri;
+// console.log(si);
+
+let ri = 5;
+let si = ri ** 2;
+console.log(si);
