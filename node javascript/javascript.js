40ee1d2187b8d01f036760e04a6cf85a4464e1d5
wieslawpaleta <tankluk@gmail.com>
2026-09-21 20:40:00 +0200

    Add files via upload

--- node javascript/javascript.js
+++ node javascript/javascript.js
@@ -1,216 +1,331 @@
-// JavaScript, Where To, Introduction, Where To, Output, Syntax, Statements, Comments, Variables, Let, Const, Datatypes, Operators, Arithmetic  (tu jestem teraz)
+// JavaScript, Where To, Introduction, Where To, Output, Syntax, Statements, Comments, Variables, Let, Const, Datatypes, Operators, Arithmetic, Assignment 
+// Operators/Comparison (tu jestem teraz)
 /* */
 
-let x = 5;
-let y = 6;
-let u = 10.50;
+// let x = 5;
+// let y = 6;
+// let u = 10.50;
 
-let z = x + y;
-let w = x + y + u;
+// let z = x + y;
+// let w = x + y + u;
 
-const fname = "John";
+// const fname = "John";
 
-console.log(z + " " + "Coucou")
-console.log(w)
+// console.log(z + " " + "Coucou")
+// console.log(w)
 
-let p;
-p = 6;
+// let p;
+// p = 6;
 
-console.log(fname + p)
+// console.log(fname + p)
 
-console.log((5 + 6) * 10)
+// console.log((5 + 6) * 10)
 
-let l, k, j;
-l = 1;
-k = 2;
-j = l + k;
-console.log(j)
-
-
-let _lastName = "Pierre";
-let _x = 2;
-let _100 = 5;
-let $ = "Bonsoir!"
-let voitureNombre;
-voitureNombre = "Peugeot";
+// let l, k, j;
+// l = 1;
+// k = 2;
+// j = l + k;
+// console.log(j)
 
 
-const prix1 = 5;
-const prix2 = 6;
-let total = prix1 + prix2;
+// let _lastName = "Pierre";
+// let _x = 2;
+// let _100 = 5;
+// let $ = "Bonsoir!"
+// let voitureNombre;
+// voitureNombre = "Peugeot";
 
 
-let personne = "Jean Pierre", nobreVoiture = "Citroen", prix3 = 300;
+// const prix1 = 5;
+// const prix2 = 6;
+// let total = prix1 + prix2;
 
 
-let op = 2 + 3 + "5";
-
-console.log(op);
-
-var gr = 10;
-
-{
-var gr = 11;
-}
-
-console.log(gr);
+// let personne = "Jean Pierre", nobreVoiture = "Citroen", prix3 = 300;
 
 
-carName = "Volvo";
-var carName;
+// let op = 2 + 3 + "5";
 
-//function myFunction() {
-//   document.getElementById("demo1").innerHTML = "Hello Dolly!";
-//   document.getElementById("demo2").innerHTML = "How are you?";
+// console.log(op);
+
+// var gr = 10;
+
+// {
+// var gr = 11;
 // }
 
-// console.log(myFunction())
+// console.log(gr);
 
 
-const voitureConst = ["Peugeot", "Citroen", "Renault"];
-voitureConst[0] = "DS";
-voitureConst.push("Alpina");
+// carName = "Volvo";
+// var carName;
 
-console.log(voitureConst);
+// //function myFunction() {
+// //   document.getElementById("demo1").innerHTML = "Hello Dolly!";
+// //   document.getElementById("demo2").innerHTML = "How are you?";
+// // }
 
-
-const voitureObject = {type:"Peugeot", model:"3008", couleur: "blanc"};
-
-voitureObject.couleur = "rouge";
-
-voitureObject.proprietaire = "Jacquouille la Fripouille";
-
-console.log(voitureObject);
+// // console.log(myFunction())
 
 
-const ao = 10;
+// const voitureConst = ["Peugeot", "Citroen", "Renault"];
+// voitureConst[0] = "DS";
+// voitureConst.push("Alpina");
 
-{
-const ao = 2;
-console.log(ao);
-}
-
-console.log(ao);
-
-var bo = 2;
-var bo = 3;
-bo = 4;
-
-console.log(bo);
+// console.log(voitureConst);
 
 
-let co = "jaune";
-let eo = 16;
-let fo = 7.5;
-let go = 1234567890123456789012345n;
-let ho = BigInt(1234567890123456789012345);
-let io = true;
-let jo = false;
-const ko = {nom:"Obelix", nomDeFamille:"Asterix"};
-const lo = ["Peugeot", "Renault", "Citroen"];
-const mo = new Date("2022-03-25");
-let no;
-let oo;
-let po = null;
-let qo = null;
-const ro = Symbol();
-const so = Symbol();
+// const voitureObject = {type:"Peugeot", model:"3008", couleur: "blanc"};
 
-console.log(so);
+// voitureObject.couleur = "rouge";
+
+// voitureObject.proprietaire = "Jacquouille la Fripouille";
+
+// console.log(voitureObject);
 
 
-typeof ""
-typeof "Gabriel"
-typeof "Dubois  Dubois"
-typeof 0
-typeof 314
-typeof 3.14
-typeof (3)
-typeof (3 + 4)
+// const ao = 10;
+
+// {
+// const ao = 2;
+// console.log(ao);
+// }
+
+// console.log(ao);
+
+// var bo = 2;
+// var bo = 3;
+// bo = 4;
+
+// console.log(bo);
 
 
-let to = 123e5;
-console.log(to);
+// let co = "jaune";
+// let eo = 16;
+// let fo = 7.5;
+// let go = 1234567890123456789012345n;
+// let ho = BigInt(1234567890123456789012345);
+// let io = true;
+// let jo = false;
+// const ko = {nom:"Obelix", nomDeFamille:"Asterix"};
+// const lo = ["Peugeot", "Renault", "Citroen"];
+// const mo = new Date("2022-03-25");
+// let no;
+// let oo;
+// let po = null;
+// let qo = null;
+// const ro = Symbol();
+// const so = Symbol();
+
+// console.log(so);
 
 
-let uo = 5;
-
-(uo == 8);
-(uo != 8);
-(uo > 8);
-(uo < 8);
-
-
-let vo;
-let wo = "";
+// typeof ""
+// typeof "Gabriel"
+// typeof "Dubois  Dubois"
+// typeof 0
+// typeof 314
+// typeof 3.14
+// typeof (3)
+// typeof (3 + 4)
 
 
-let xo = 10;
-let yo = 2;
-let zo = xo + yo;
-
-console.log(zo);
+// let to = 123e5;
+// console.log(to);
 
 
-let ai = 5;
-let bi = 2;
-let ci = ai * bi;
+// let uo = 5;
+
+// (uo == 8);
+// (uo != 8);
+// (uo > 8);
+// (uo < 8);
 
 
-let di = 3;
-let ei = (100 * 50) * di;
+// let vo;
+// let wo = "";
 
 
-let fi = "Jean";
-let gi = "Rogue";
-let hi = fi + " " + gi;
+// let xo = 10;
+// let yo = 2;
+// let zo = xo + yo;
 
-console.log(hi);
-
-
-let ii = "Quelle ";
-ii += "belle journée !";
-console.log(ii)
+// console.log(zo);
 
 
-let ji = 5 + 5;
-let ki = "5" + 5;
-let li = "Coucou" + 5;
-console.log(ji);
-console.log(ki);
-console.log(li);
+// let ai = 5;
+// let bi = 2;
+// let ci = ai * bi;
 
 
-let mi = 10;
-mi += 5;
-console.log(mi);
+// let di = 3;
+// let ei = (100 * 50) * di;
 
 
-let ni = 5;
-let resultat = ni > 8;
-console.log(resultat);
+// let fi = "Jean";
+// let gi = "Rogue";
+// let hi = fi + " " + gi;
+
+// console.log(hi);
 
 
-let oi = "A";
-let pi = "B";
-let resultat1 = oi === pi;
-console.log(resultat1);
+// let ii = "Quelle ";
+// ii += "belle journée !";
+// console.log(ii)
 
 
-let qi = mi % ni ;
-console.log(qi);
+// let ji = 5 + 5;
+// let ki = "5" + 5;
+// let li = "Coucou" + 5;
+// console.log(ji);
+// console.log(ki);
+// console.log(li);
 
+
+// let mi = 10;
+// mi += 5;
+// console.log(mi);
+
+
+// let ni = 5;
+// let resultat = ni > 8;
+// console.log(resultat);
+
+
+// let oi = "A";
+// let pi = "B";
+// let resultat1 = oi === pi;
+// console.log(resultat1);
+
+
+// let qi = mi % ni ;
+// console.log(qi);
+
+
+// // let ri = 5;
+// // ri++;
+// // let si = ri;
+// // console.log(si);
+
+// // let ri = 5;
+// // ri--;
+// // let si = ri;
+// // console.log(si);
+
+// // let ri = 5;
+// // let si = ri ** 2;
+// // console.log(si);
 
 // let ri = 5;
-// ri++;
-// let si = ri;
+// let si = Math.pow(x,2);
 // console.log(si);
 
-// let ri = 5;
-// ri--;
-// let si = ri;
-// console.log(si);
 
-let ri = 5;
-let si = ri ** 2;
-console.log(si);
+// let ti = (100 + 50) * 3;
+// let ui = 100 + 50 * 3;
+// let vi = 100 + 50 - 3;
+// console.log(ti);
+// console.log(ui);
+// console.log(vi);
+
+
+// let wi = 10;
+// let xi = wi;
+// console.log(xi);
+
+// let wi = 10;
+// wi += 5;
+// console.log(wi);
+
+// let wi = 10;
+// wi -= 5;
+// console.log(wi);
+
+// let wi = 10;
+// wi *= 5;
+// console.log(wi);
+
+// let wi = 10;
+// wi **= 5;
+// console.log(wi);
+
+// let wi = 10;
+// wi /= 5;
+// console.log(wi);
+
+// let wi = 10;
+// wi %= 5;
+// console.log(wi);
+
+// let untext = "Bonjour";
+
+// let untext = "Bonjour"; untext += " tout le monde";
+// console.log(untext);
+
+// let x = true;
+// let y = x &&= 10;
+// console.log(y);
+
+// let x = false;
+// let y = x &&= 10;
+// console.log(y);
+
+// let x = 1;
+// let y = x &&= 10;
+// console.log(y);
+
+// let x = 0;
+// let y = x &&= 10;
+// console.log(y);
+
+// let x = undefined;
+// let y = x &&= 10;
+// console.log(y);
+
+// let x = null;
+// let y = x &&= 10;
+// console.log(y);
+
+// let x = false;
+// let y = x ||= 10;
+// console.log(y);
+
+// let x = true;
+// let y = x ||= 10;
+// console.log(y);
+
+// let x = null;
+// let y = x ||= 10;
+// console.log(y);
+
+// let x = undefined;
+// let y = x ||= 10;
+// console.log(y);
+
+// let x;
+// x ??= 10;
+// console.log(x);
+
+// let x = 0;
+// x ??= 10;
+// console.log(x);
+
+// let x = null;
+// x ??= 10;
+// console.log(x);
+
+// let x = undefined;
+// x ??= 10;
+// console.log(x);
+
+// let x = 10;
+// let y = 5;
+// x ??= 10;
+// console.log(x)
+
+let text = "12345";
+
+let min = Math.min(...text);
+let max = Math.max(...text);
+console.log(min);
+console.log(max);
